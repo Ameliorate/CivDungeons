3fdbf09208ade5877e91ac7ae80be973421aa227
Amelorate <ameilorate2@gmail.com>
2018-12-08 16:15:58 -0800

    Make the testserver script symlink the plugin into the plugins folder
    It's kinda untested but I don't see anywhere where it can go wrong.

--- scripts/createtestserver.sh
+++ scripts/createtestserver.sh
@@ -8,6 +8,7 @@ if [ -d "$gitroot/testserver" ]; then
   exit
 fi
 
+mvn package
 mkdir "$gitroot/testserver"
 wget -O "$gitroot/testserver/BuildTools.jar" https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 git config --global --unset core.autocrlf
@@ -15,3 +16,5 @@ cd "$gitroot/testserver" # cd into testserver to work around buildtools placing 
                          # the dir buildtools is in.
 java -jar "$gitroot/testserver/BuildTools.jar" -rev 1.12.2
 cd "$currentdir"
+mkdir "$gitroot/testserver/plugins"
+ln -s "$gitroot/target/DungeonMod.jar" "$gitroot/testserver/plugins/DungeonMod.jar"
